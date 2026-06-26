23c8633ba9ca0e46b5d30c72b2733feae2f6178e
Yashvardhan singh Rathore <yashvardhan2797@gmail.com>
2026-06-26 19:30:08 +0530

    day 3

---
+++ day 3.c/count the digit in a no.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        num = num / 10;
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}
---
+++ day 3.c/reverse a no.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, reverse = 0, rem;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        rem = num % 10;
+        reverse = reverse * 10 + rem;
+        num = num / 10;
+    }
+
+    printf("Reverse = %d", reverse);
+
+    return 0;
+}
